645db7435097f8043cfcc6d79f3ac787264e1aee
Deepesh krishnan <deepesh.krishnan@gmail.com>
2017-03-06 12:14:33 +0530

    updated with validation

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -12,7 +12,24 @@ RSpec.feature "Creating Articles" do
     click_button "Create Article"
     
     expect(page).to have_content "Article has been created"
+    # move to new page
     expect(page.current_path).to  eq(articles_path)
+    
+  end
+  
+  scenario "Fails in creating new article" do
+    visit "/" 
+    
+    click_link "New Article"
+    
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+    
+    click_button "Create Article"
+    
+    expect(page).to have_content "Article has not been created"
+    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_content "Body can't be blank"
     
   end
 
